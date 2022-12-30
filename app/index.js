acc75e3dc10c7741c0a4d7efaf1358f252b1584f
Quinn Shultz <vanoxite@gmail.com>
2022-12-30 13:57:26 -0500

    Implemented basic summoner timer functionality.

--- app/index.js
+++ app/index.js
@@ -1,7 +1,31 @@
 /*
  * Entry point for the watch app
  */
-import * as document from "document";
+import * as document from "document"
+let timers = document.getElementsByClassName("timers")
 
-let demotext = document.getElementById("demotext");
-demotext.text = "LoL Jungle Timers";
+const SUMMONER_SPELL_TIME = 50000; // 5 minutes
+
+// Get click event for each button
+timers.forEach((timer) => {
+  timer.addEventListener("click", (evt) => {
+    startTimer(timer)
+  });
+});
+
+async function startTimer(timer) {
+  // Change the Flash background to Ignite
+  timer.href="Ignite.jpg"  
+  
+  console.log(timer.id + ' started')
+  setTimeout(function() {
+    console.log(timer.id + ' finished')
+    stopTimer(timer)
+  }, SUMMONER_SPELL_TIME); // Wait
+  
+}
+
+function stopTimer(timer) {
+  // Change the Ignite background to Flash
+  timer.href="Flash.jpg"
+}
